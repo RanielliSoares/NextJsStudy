b4b57c7c3089443d64d397b882316e01a164ee44
Ranielli Pinho <ranielli.pinho@outlook.com>
2025-04-06 00:37:48 -0300

    server component

---
+++ meuprojeto/src/app/contatos/page.tsx
@@ -0,0 +1,8 @@
+export default function Contatos(){
+    return(
+        <div>
+            <h1>Página de Contatos</h1>
+            <span>(00) 00000-0000</span>
+        </div>
+    )
+}
---
+++ meuprojeto/src/app/page.tsx
@@ -0,0 +1,35 @@
+interface DataProps{
+  id: number;
+  name: string;
+  full_name: string;
+  owner:{
+    login: string;
+    id: number;
+    avatar_url: string;
+    url: string;
+  }
+}
+
+async function getData(){
+  const response = await fetch("https://api.github.com/users/RanielliSoares/repos")
+  return response.json();
+}
+
+export default async function Home(){
+  const data: DataProps[] = await getData();
+
+  return(
+    <main>
+      <h1>Página Home</h1>
+      <span>Seja bem vindo </span>
+      <br/>
+      <h3>Meus repositórios</h3>
+      {data.map((item) => (
+        <div key={item.id}>
+          <strong>Repositório: </strong><a>{item.name}</a>
+          <br/><br/>
+        </div>
+      ))}
+    </main>
+  )
+}
